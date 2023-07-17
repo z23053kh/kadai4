feeb98d289758dade5b39ab91b4b74614a42f0eb
z23053kh <z23053kh@aiit.ac.jp>
2023-07-17 14:58:15 +0000

    テストケースを追加

--- test.sh
+++ test.sh
@@ -5,14 +5,25 @@ NC='\033[0m'
 success_message="${SUCCESS_GREEN}成功${NC}"
 error_message="${ERROR_RED}エラー${NC}"
 
-count=0
+total_count=7
+passed_count=0
 
 # 正しくパスすること
 test_case=$(./gcd.sh 2 4)
 expectation=2
 if [[ $test_case -eq $expectation ]]; then
   echo -e $success_message
-  count=$(expr $count + 1)
+  passed_count=$(expr $passed_count + 1)
+else
+  echo -e $error_message
+  exit 1
+fi
+
+test_case=$(./gcd.sh 30 12)
+expectation=6
+if [[ $test_case -eq $expectation ]]; then
+  echo -e $success_message
+  passed_count=$(expr $passed_count + 1)
 else
   echo -e $error_message
   exit 1
@@ -22,7 +33,7 @@ fi
 ./gcd.sh 3 | grep "引数は2つ指定してください"
 if [[ $? -eq 0 ]]; then
   echo -e $success_message
-  count=$(expr $count + 1)
+  passed_count=$(expr $passed_count + 1)
 else
   echo -e $error_message
   exit 1
@@ -31,7 +42,7 @@ fi
 ./gcd.sh 38 5 10 | grep "引数は2つ指定してください"
 if [[ $? -eq 0 ]]; then
   echo -e $success_message
-  count=$(expr $count + 1)
+  passed_count=$(expr $passed_count + 1)
 else
   echo -e $error_message
   exit 1
@@ -41,7 +52,16 @@ fi
 ./gcd.sh -67 2 | grep "引数は自然数としてください"
 if [[ $? -eq 0 ]]; then
   echo -e $success_message
-  count=$(expr $count + 1)
+  passed_count=$(expr $passed_count + 1)
+else
+  echo -e $error_message
+  exit 1
+fi
+
+./gcd.sh 112.5 84 | grep "引数は自然数としてください"
+if [[ $? -eq 0 ]]; then
+  echo -e $success_message
+  passed_count=$(expr $passed_count + 1)
 else
   echo -e $error_message
   exit 1
@@ -50,11 +70,11 @@ fi
 ./gcd.sh 阿 🐍 | grep "引数は自然数としてください"
 if [[ $? -eq 0 ]]; then
   echo -e $success_message
-  count=$(expr $count + 1)
+  passed_count=$(expr $passed_count + 1)
 else
   echo -e $error_message
   exit 1
 fi
 
-echo $count 個のテストが成功しました
+echo $passed_count/$total_count 個のテストが成功しました
 
