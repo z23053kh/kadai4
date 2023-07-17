1bc34b40ed33f818610dc28113a5a4e5183a4e33
z23053kh <z23053kh@aiit.ac.jp>
2023-07-17 12:50:41 +0000

    initial commit

---
+++ gcd.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function is_natural_number() {
+  num1=$1
+  num2=$2
+  regex='^[0-9]+$'
+
+  if [[ ! $num1 =~ $regex || ! $num2 =~ $regex ]]; then
+    echo '引数は自然数としてください'
+    exit 1
+  fi
+}
+
+function calculate_gcd() {
+  num1=$1
+  num2=$2
+
+  while [[ $num2 -ne 0 ]]; do
+    remainder=$((num1 % num2))
+    num1=$num2
+    num2=$remainder
+  done
+
+  echo $num1
+}
+
+if [[ $# -eq 2 ]]; then
+  is_natural_number $1 $2
+  calculate_gcd $1 $2
+else
+  exho '引数は2つ指定してください'
+  exit 1
+fi
+
